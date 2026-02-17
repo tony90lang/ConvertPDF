4276089a1dc528638a063fd31a34af7c0f304de0
Anant Demanna <anant22e@gmail.com>
2026-02-17 20:00:02 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- script.js
+++ script.js
@@ -98,7 +98,7 @@
         container.appendChild(area);
 
     // ---------- IMPROVED: Markdown → PDF ----------
-else if (toolId === 'md2pdf') {
+if (toolId === 'md2pdf') {
     area.innerHTML = `
         <h3>📂 Upload .md file</h3>
         <div class="flex-row">
@@ -683,3 +683,4 @@ else if (toolId === 'md2pdf') {
     }
 
 })();
+
